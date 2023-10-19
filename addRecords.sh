06f02b8e0da4278d8ca0841b3634d28cd78682a3
Pat Walsh-O'Connor <114754356+chipspeak@users.noreply.github.com>
2023-10-19 23:35:33 +0100

    Bug fix for running on Rasberry Pi
    Encountered an issue where regex in addRecords caused incorrect behaviour when running on ubuntu on a pi. Changed regex from previous version to letterRestriction="^[[:alpha:]]+$" and issue has been resolved

--- addRecords.sh
+++ addRecords.sh
@@ -3,124 +3,275 @@
 ##========================================================
 ##Author: Patrick O'Connor
 ##Student number: 20040412
-##github: https://github.com/chipspeak
+##github: https://github.com/chipspeak/compSysAssignment1
 ##Description: script to add to new information to records
 ##========================================================
 
-##variable to be set to true for use in a do while loop
 
+##==========================================================================================================================================================
+##VARIABLES
+##==========================================================================================================================================================
+
+
+##variables for use in while loops.
 subMenu=true
 question=true
+instrumentCheck=false
 confirmation=false
+check=false
+
+##variables for later use to check for letters or numbers via regular expressions.
 numRestriction="^[0-9]{9}$"
-letterRestriction="^[a-zA-z]+$"
+letterRestriction="^[[:alpha:]]+$"
 
+##variable used to store the regex that will be used to validate email addresses
+emailRestriction='^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$'
+
+##an array of counties for use in error checking when adding a county.
+counties=("clare" "cork" "kerry" "limerick" "tipperary" "waterford" "carlow" "dublin" "kildare" "kilkenny" "laois" "longford" "louth" "meath" "offaly" "westmeath" "wexford" "wicklow" "galway" "leitrim" "mayo" "roscommon" "sligo" "antrim" "armagh" "tyrone" "derry" "down" "donegal" "fermanagh" "monaghan" "cavan")
+
+##variables for use in colour and text formatting
+GREEN=$'\e[32m'
+RED=$'\e[31m'
+YELLOW=$'\e[33m'
+CYAN=$'\e[96m'
+NC=$'\e[0m'
+INVERTED=$'\e[7m'
+UNDERLINED=$'\e[4m'
+BOLD=$'\e[1m'
+
+
+##==========================================================================================================================================================
+##FUNCTIONS
+##==========================================================================================================================================================
+
+
+##function using tput command in addition to columns to substitude equals sign into character width of screen.
 spacing() {
-        echo " "
-        printf "%*s" $(tput cols) | tr " " "=\n"
-        echo " "
+    echo " "
+    printf "%*s" $(tput cols) | tr " " "=\n"
+    echo " "
 }
 
+##function for use in breaking up user experience via brief load and clear.
+loading() {
+    spacing
+    echo "${CYAN}Loading...${NC}"
+    spacing
+    sleep 1
+    clear
+}
+
+##function to echo a message to user notifying them of menu change prior to clear and return.
 exit_menu() {
-        spacing
-        echo "returning to the previous menu"
-        spacing
-        sleep 1
-        clear
-        exit
+    spacing
+    echo "${BOLD}${CYAN}returning to the previous menu${NC}"
+    spacing
+    sleep 1
+    clear
+    exit
 }
 
+##function which takes in a user input as a variable and checks for return option, regex letter check and blank space check.
 detail_check() {
-if [ "$1" = "r" ] || [ "$1" = "R" ] ; then
+    if [ "$1" = "r" ] || [ "$1" = "R" ] ; then
         exit_menu
-elif [ "$1" != "" ] && [[ $1 =~ $letterRestriction ]]; then
+    elif [ "$1" != "" ] && [[ $1 =~ $letterRestriction ]] ; then
         question=false
-else echo "This field cannot contain numbers or symbols. Please enter only letters"
-fi
+    else echo "${YELLOW}This field cannot contain numbers, symbols or be left blank. Please enter only letters${NC}"
+    fi
 }
 
+##function for use with final stage of email input. It checks that the input is appropriate via regex before returning
+##a 0 for success or 1 for failure for use in the final check.
 email_check() {
-if [ "$1" = "" ] ; then
-echo "This field cannot be left blank. "
-else question=false
-fi
+    if [[ $1 =~ $emailRestriction ]]; then
+        return 0 
+    else
+        return 1 
+    fi
 }
 
+##similar function to detail check but this regex is used to ensure only numbers are entered.
 number_check() {
-if [ "$1" = "r" ] || [ "$1" = "R" ] ; then
+    if [ "$1" = "r" ] || [ "$1" = "R" ] ; then
         exit_menu
-elif [ "$1" != "" ] && [[ $1 =~ $numRestriction ]] ; then
+    elif [ "$1" != "" ] && [[ $1 =~ $numRestriction ]] ; then
         question=false
-else echo "Please enter the 9 digits following +353. This field cannot be left blank. "
-fi
+    else echo "${YELLOW}Please enter the 9 digits following +353. This field cannot be left blank. ${NC}"
+    fi
 }
 
+##function to check that the user has not entered an inappropriate value or blank space in the speciality field.
+speciality_check() {
+    if [ "$1" = "" ] ; then
+        echo "${YELLOW}Invalid entry. Please enter one of the numeric options provided. ${NC}"
+    elif [ "$1" = "R" ] || [ "$1" = "r" ] ; then
+    exit_menu
+    else instrumentCheck=false
+    fi
+}
+
+##function to check the user input matches a county within the county array.
+##two variables are used as switches here to allow the loop to be exited if the input matches a county in the array
+##the second variable is used in the event that the input fails to match anything in the array.
+##the check is then set to false. External to the loop, an if statement is used in conjunction with the check variable
+##if check is false, an error is echoed to the user explaining that the input is invalid.
+county_check() {
+    for i in "${counties[@]}"
+    do
+        if [ "${1,,}" = "$i" ] ; then ##using a comma as part of the parameter containing the passed in variable to ensure case match to array
+            check=true
+            question=false
+            break
+        elif [ "$1" = "r" ] || [ "$1" = "R" ] ; then
+            exit_menu
+        elif [ "$1" != "$i" ] ; then
+            check=false
+        fi
+    done
+    if [ "$check" = false ] ; then
+    echo "${YELLOW}Please enter a valid Irish county, being mindful of spelling errors or blank space.${NC}"
+    fi
+}
+
+##as throughout these scripts, this function creates a new file, adds the users input to it with comma delimiters as appropriate
+##and then uses sed to supply headings and then outputs to the user using column for formatting before deleting the new file.
+present_entry() {
+        echo "$name,$company,$speciality,$city,$county,+353$phone,$email" > searchresult.txt | column -t -s ","
+        ##once again the sed command are used to present the headings appropriately in new file
+        sed '1 i\NAME,COMPANY,SPECIALTY,CITY,COUNTY,PHONE,EMAIL' searchresult.txt | column -t -s ","
+        ##as we've used grep to visually output the contents of the new file, it is subsequently deleted
+        rm searchresult.txt
+}
+
+##function for use in confirming entered information. The user has had the details echoed back. A y or n option is then given to them
+##this function also has a nested if statement within the yes option which uses grep to search the document for a matching entry.
+##if a matching entry is found the user is informed of this and the loop is broken. Otherwise the entry is added to the txt file.
 yes_or_no() {
-confirmation=false
-while ! $confirmation; do
-read -p "Is the above information correct(Y/N)? " answer
+    confirmation=false
+    while ! $confirmation; 
+    do
+        read -p "Is the above information correct(${GREEN}${BOLD}Y${NC}/${RED}${BOLD}N${NC})? " answer
         if [ $answer = "Y" ] || [ $answer = "y" ] ; then
-        spacing
-        echo "Adding details to records..."
-        echo "$name,$company,$speciality,$city,$county,+353$phone,$email" >> records.txt
-        sleep 1
-        echo "Details added"
-        confirmation=true
+            spacing
+            check=$(grep -i -w -F "$name,$company,$speciality,$city,$county,+353$phone,$email" records.txt)
+            if [ "$check" ] ; then
+                echo "${YELLOW}These details are already present in the records. ${NC}"
+                break
+            else
+                echo "$name,$company,$speciality,$city,$county,+353$phone,$email" >> records.txt
+            fi
+            echo "Adding details to records..."
+            sleep 1
+            echo "${GREEN}${BOLD}Details added${NC}"
+            sleep 2
+            clear
+            confirmation=true
         elif [ $answer = "N" ] || [ $answer = "n" ] ; then
-        confirmation=true
-        else echo "Invalid input. Please answer 'Y' or 'N' "
-fi
-done
+            echo "Restarting entry process..."
+            sleep 2
+            clear
+            confirmation=true
+        else echo "${YELLOW}Invalid input. Please answer'Y' or 'N' ${NC}"
+        fi
+    done
 }
 
-##outer loop is set
-while $subMenu; do
-spacing
-##user is prompted to enter the record details one at a time or r to return. This is then stored in the appropriate variable.
-question=true
-while $question; do
-read -p "Enter the name of the rep you wish to add or enter 'R' to return: " name
-detail_check $name
-done
-question=true
-while $question; do
-read -p "Enter the rep's company or enter 'R' to return: " company
-detail_check $company
-done
-question=true
-while $question; do
-read -p "Enter the speciality of the company or enter 'R' to return: " speciality
-detail_check $speciality
-done
-question=true
-while $question; do
-read -p "Enter the city in which the rep is based or enter 'R' to return: " city
-detail_check $city
-done
-question=true
-while $question; do
-read -p "Enter the county in which the rep is based or enter 'R' to return: " county
-detail_check $county
-done
-question=true
-while $question; do
-read -p "Enter the rep's phone number or enter 'R' to return: " phone
-number_check $phone
-done
-question=true
-while $question; do
-read -p "Enter the email address of the rep or enter 'R' to return: " email
-email_check $email
-done
-spacing
-##user then has their input echoed back to them before being given a prompt before adding it to the txt file
-echo "NAME:$name
-COMPANY:$company
-SPECIALTY:$speciality
-CITY:$city
-COUNTY:$county
-PHONE:$phone
-EMAIL:$email"
-spacing
-yes_or_no $name $company $speciality $city $county $phone $email
+
+##==========================================================================================================================================================
+##MAIN PROGRAMME
+##==========================================================================================================================================================
+
+
+##outer menu loop is set
+loading
+while $subMenu; 
+do
+    spacing
+    echo "${INVERTED}${CYAN} A D D - A - N E W - R E C O R D ${NC}"
+    echo ""
+    ##user is prompted to enter the record details one at a time or r to return. These are then stored in the appropriate variables.
+    ##then the check functions have these new variables passed into them for error checking as explained above. 
+    ##the loops ensure that correct information must be entered before progressing through the system.
+    ##the question variable is set back to true before each loop given the check functions set it to false upon success.
+    question=true
+    while $question; 
+    do
+        read -p "Enter the name of the rep you wish to add or enter 'R' to return: " name
+        detail_check $name
+    done
+    question=true
+    while $question; 
+    do
+        read -p "Enter the rep's company or enter 'R' to return: " company
+        detail_check $company
+    done
+    question=true
+    instrumentCheck=true
+    ##below is a loop used to ensure that the user cannot enter innapropriate specialities via a case.
+    ##the presented choices are appropriate and error checking is then easily performed.
+    while $instrumentCheck; 
+    do
+        echo "Please select the instrumental specialty of the rep you wish to add "
+        echo "1) Guitar"
+        echo "2) Piano"
+        echo "3) String"
+        echo "4) Wind"
+        echo "5) Percussion"
+        echo "6) Brass"
+        echo "7) Return to the previous menu"
+        read answer
+        case $answer in
+            1) speciality="Guitar";;
+            2) speciality="Piano";;
+            3) speciality="String";;
+            4) speciality="Wind";;
+            5) speciality="Percussion";;
+            6) speciality="Brass";;
+            7) speciality="R";;
+            *) speciality=""
+        esac
+        speciality_check $speciality
+    done
+    question=true
+    while $question; 
+    do
+        read -p "Enter the city in which the rep is based or enter 'R' to return: " city
+        detail_check $city
+    done
+    question=true
+    while $question; 
+    do
+        read -p "Enter the county in which the rep is based or enter 'R' to return: " county
+        county_check $county
+    done
+    question=true
+    while $question; 
+    do
+        read -p "Enter the rep's phone number or enter 'R' to return: " phone
+        number_check $phone
+    done
+    question=true
+    ##this final check is vital as it is how the remove feature will be implemented
+    ##if the email is anything other than the correct format or the letter r to return, an error message is echoed.
+    while $question; 
+    do
+        read -p "Enter the email address of the rep or enter 'R' to return: " email
+        ##passes the user input to the email_check for function for regex comparison. If a 0 is returned (success) loop is exited.
+        if email_check "$email" ; then
+        question=false
+        ##letter r input is also accounted for lest the user wish to return to the previous menu.
+        elif [ "$email" = "r" ] || [ "$email" = "R" ] ; then
+            exit_menu
+        ##otherwise an error message is echoed to the user prompting them to use the correct email format
+        else echo "${YELLOW}Please enter a valid email in the format of example@example.com${NC}"
+        fi
+    done
+    spacing
+    ##user then has their input echoed back to them before being given a prompt before adding it to the txt file
+    present_entry $name $company $speciality $city $county $phone $email
+    spacing
+    ##finally these variables are passed to the yes_or_no function.
+    yes_or_no $name $company $speciality $city $county $phone $email
 done
--- remove.sh
+++ remove.sh
@@ -3,79 +3,271 @@
 ##=================================================================
 ##Author: Patrick O'Connor
 ##Student number: 20040412
-##github: https://github.com/chipspeak
+##github: https://github.com/chipspeak/compSysAssignment1
 ##Description: script to filter through txt file and remove records
 ##=================================================================
 
-##variable purely for aesthetic adjustments.
+
+##==========================================================================================================================================================
+##VARIABLES
+##==========================================================================================================================================================
 
 
-
+##variable used in the yes or no method loop
 confirmation=false
-entry=false
-headings="NAME,COMPANY,SPECIALITY,CITY,COUNTY,PHONE,EMAIL"
+##variable to be set to true for use in the outer loop of this script
+subMenu=true
+##variable for use within inner menu loops
+removeMenu=true
+##variable for use in speciality choice menu
+speciality=""
+##variable used to store the regex that will be used to validate email addresses
+emailRestriction='^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$'
 
+##colour and text formatting variables
+GREEN=$'\e[32m'
+RED=$'\e[31m'
+YELLOW=$'\e[33m'
+CYAN=$'\e[96m'
+NC=$'\e[0m'
+INVERTED=$'\e[7m'
+UNDERLINED=$'\e[4m'
+BOLD=$'\e[1m'
+
+
+##==========================================================================================================================================================
+##FUNCTIONS
+##==========================================================================================================================================================
+
+
+##function using tput command in addition to columns to substitude equals sign into character width of screen
 spacing() {
-        echo " "
-        printf "%*s" $(tput cols) | tr " " "=\n"
-        echo " "
+    echo " "
+    printf "%*s" $(tput cols) | tr " " "=\n"
+    echo " "
 }
 
+##function for use in breaking up user experience via brief load and clear.
+loading() {
+    spacing
+    echo "${BOLD}${CYAN}Loading...${NC}"
+    spacing
+    sleep 1
+    clear
+}
+
+##function similar to loading but for returning to other menus
+returning() {
+    spacing
+    echo "${BOLD}${CYAN}Returning to the previous menu...${NC}"
+    spacing
+    sleep 1
+    clear
+}
+
+##function which uses the same concept as the welcome menu but in the context of displaying an error message
+##for use in conjunction with menu options
+display_error() {
+    clear
+    spacing
+    echo "${YELLOW}Invalid option. Please enter one of the supplied numbers. ${NC}"
+    spacing
+    read -n 1 -s -r -p "${BOLD}${CYAN}Press any key to try again ${NC}" ans
+    clear
+}
+
+##function which uses regex to check if the email passed in matches the appropriate format before returning a success or failure binary
+email_check() {
+    emailSearch=$(grep -i -w -F "$1" records.txt)
+    if [[ $1 =~ $emailRestriction ]] && [ "$emailSearch" ] ; then
+        return 0 
+    else
+        return 1 
+    fi
+}
+
+##function which allows user to remove multiple records by filtering via county
+remove_by_county() {
+    clear
+    while [ $removeMenu ]
+        do
+        spacing
+        read -p "Please enter the county you wish to remove from the records or press 'R' to return to the previous menu: " county
+        if [ "$county" = "r" ] || [ "$county" = "R" ] ; then
+            returning
+            break
+        elif [ "$county" != "r" ] || [ "$county" != "R" ] && [ "$county" != "" ]; then
+            remove_multiple_records $county
+        elif [ "$county" = "" ] ; then
+            echo "${YELLOW}This field cannot be left blank. Please enter again. ${NC}"
+        fi
+    done
+}
+
+##function which allows the user to remove a single record via email
+remove_single_record() {
+    clear
+    while [ $removeMenu ]
+        do
+        spacing
+        ##sed command is used to add the headings to the output of the txt file. This is piped into column for formatting via comma delimiter.
+        sed '1 i\NAME,COMPANY,SPECIALTY,CITY,COUNTY,PHONE,EMAIL' records.txt | column -t -s ","
+        spacing
+        ##user is prompted to enter the details they wish to search for or r to return. This is then stored in the input variable.
+        read -p "Please enter the ${UNDERLINED}email address${NC} of the rep you wish to remove or enter 'R' to return: " input
+        ##if variable is r returning function is called and loop is broken
+        if [ "$input" = "r" ] || [ "$input" = "R" ] ; then
+            returning
+            break
+        ##if statement checks if the function is returning a true (i.e 0) when using a regex check on the input email and then executes the following commands.
+        elif  email_check "$input" ; then
+            spacing
+            ##grep is used to place the result in a new txt file for formatting with headings and columns
+            grep -i -w -F "$input" records.txt >> searchresult.txt | column -t -s ","
+            ##once sed command is used to present the headings appropriately in new file
+            sed '1 i\NAME,COMPANY,SPECIALTY,CITY,COUNTY,PHONE,EMAIL' searchresult.txt | column -t -s ","
+            ##variable using wc -l to check line numbers of the new file and used to present user with number of potentially deleted records
+            removeNum=$(cat searchresult.txt | wc -l)
+            ##as we've used grep to visually output the contents of the new file, it is subsequently deleted
+            rm searchresult.txt
+            spacing
+            ##user is then asked if they wish to delete the found records via the yes or no function
+            yes_or_no $input $removeNum
+        else clear
+            spacing
+            echo "${YELLOW}Invalid entry. Please enter one of the displayed email addresses ${NC}"
+            spacing
+            read -n 1 -s -r -p "${BOLD}${CYAN}Press any key to display the list and try again ${NC}" ans
+            clear
+        fi
+    done
+}
+
+##function which takes in a user-supplied variable via a separate function and provides the same temporary visual output seen elsewhere in the script
+remove_multiple_records() {
+    spacing
+    echo "${BOLD}${CYAN} Checking records... ${NC}"
+    spacing
+    sleep 1
+    clear
+    search=$(grep -i -w -F "$1" records.txt)
+    if [ "$search" ] ; then
+        spacing
+        ##grep is used to place the result in a new txt file for formatting with headings and columns
+        grep -i -w -F "$1" records.txt >> searchresult.txt | column -t -s ","
+        ##once sed command is used to present the headings appropriately in new file
+        sed '1 i\NAME,COMPANY,SPECIALTY,CITY,COUNTY,PHONE,EMAIL' searchresult.txt | column -t -s ","
+        ##variable using wc -l to check line numbers of the new file and used to present user with number of potentially deleted records
+        removeNum=$(cat searchresult.txt | wc -l)
+        ##as we've used grep to visually output the contents of the new file, it is subsequently deleted
+        rm searchresult.txt
+        spacing
+        ##user is then asked if they wish to delete the found records via the yes or no function
+        yes_or_no $1 $removeNum
+    else spacing   
+        echo "${INVERTED}${RED} We found no matching results to remove ${NC}"
+    fi
+}
+
+##function using a while loop and case to provide the user with the list of specialities and the option to remove via these
+##once user selects an option it is passed to the remove_multiple_records function
+remove_by_speciality() {
+    while [ $removeMenu ] 
+    do
+        ##variable reset at the start of each loop to prevent interference with display_error
+        speciality=""
+        spacing
+        echo "${INVERTED}${CYAN} Please select the instrumental specialty you to remove records by: ${NC}"
+        echo ""
+        echo "1) Guitar"
+        echo "2) Piano"
+        echo "3) String"
+        echo "4) Wind"
+        echo "5) Percussion"
+        echo "6) Brass"
+        echo "7) Return to the previous menu"
+        read answer
+        case $answer in
+            1) speciality="Guitar";;
+            2) speciality="Piano";;
+            3) speciality="String";;
+            4) speciality="Wind";;
+            5) speciality="Percussion";;
+            6) speciality="Brass";;
+            7) returning
+               break;;
+            *) display_error
+               ;;
+        esac
+        if [ $speciality != "" ] ; then 
+            remove_multiple_records $speciality
+        else clear
+        fi
+    done
+}
+
+
+##function which uses a while loop to check if the user wants to delete the results of the search.
 yes_or_no() {
 confirmation=false
-while ! $confirmation; do
-read -p "Do you wish to remove the above information from records (Y/N)? " answer
-        if [ "$answer" = "Y" ] || [ "$answer" = "y" ] ; then
-        sed -i "/$1/d" records.txt
-        echo "Removing records containing $1..."
-        sleep 1
-        echo "Record removed. "
-        sleep 1
-        clear
-        confirmation=true
-        elif [ "$answer" = "N" ] || [ "$answer" = "n" ] ; then
-        clear
-        confirmation=true
-        else echo "Invalid input. Please answer 'Y' or 'N' "
-fi
+##while the confirmation variable is false (which it is by default) prompts the user a y or n option.
+##this option, if affirmed, uses the passed argument and sed to delete the search result from the txt file.
+##a success message is echoed to the user and then the loop is broken when confirmation is declared true
+##otherwise, if the user chooses n, the screen is cleared and the loop is broken. 
+##error checking is also in place to ensure that all answers that are not y or n return an error message.
+while ! $confirmation; 
+do
+    read -p "You are about to ${UNDERLINED}remove${NC} ${INVERTED}${BOLD} $2 ${NC} record(s). Do you still want to proceed? (${GREEN}${BOLD}Y${NC}/${RED}${BOLD}N${NC})? " answer
+    if [ "$answer" = "Y" ] || [ "$answer" = "y" ] ; then
+            sed -i "/$1/d" records.txt
+            echo -e "Removing the above record(s)..."
+            sleep 1
+            echo "${GREEN}${BOLD}Record(s) removed${NC} "
+            sleep 2
+            clear
+            confirmation=true
+    elif [ "$answer" = "N" ] || [ "$answer" = "n" ] ; then
+            loading
+            break
+    else echo "${YELLOW}Invalid input. Please answer 'Y' or 'N'${NC} "
+    fi
 done
 }
 
-##variable to be set to true for use in a do while loop
-subMenu=true
-##loop is set
-while $subMenu; do
-spacing
-sed '1 i\NAME,COMPANY,SPECIALTY,CITY,COUNTY,PHONE,EMAIL' records.txt | column -t -s ","
-spacing
-##user is prompted to enter the name of choice or r to return. This is then stored in the name variable.
-read -p "Please enter the word or number you wish to search for and remove or enter 'R' to return: " input
-while [ "$input" = "" ] ; do
-        echo "Invalid entry. This field cannot be left blank "
-        read -p "Please enter the word or number you wish to search for and remove or enter 'R' to return: " input
-done
 
-if [ "$input" = "r" ] || [ "$input" = "R" ] ; then
-        echo "Returning to the previous menu..."
-        spacing
-        sleep 1
-        clear
-        exit
-fi
-## expenses variable uses grep -i -w to check for the name in the txt file case-insensitive
-## seeking a whole word before piping into awk to print the expenses column
-search=$(grep -i -w -F "$input" records.txt)
+##==========================================================================================================================================================
+##MAIN PROGRAMME
+##==========================================================================================================================================================
 
-##if statement checks if the variable is true i.e the name search returns a true result
-if [ "$search" ] ; then
-## echos a message to user listing the name and their expenses
-        spacing
-        echo "$search"
-        spacing
-        yes_or_no $input
-elif [ ! "$search" ] && [ "$input" != "r" ] && [ "$input" != "R" ] ; then
-## otherwise returns that the name could not be found and the main script returns to the initial 3 options.
-        spacing
-        echo "No results."
-fi
+
+loading
+##the outer loop in which the bulk of this scripts actions are contained
+while [ $subMenu ]
+do
+    spacing
+    echo "${INVERTED}${CYAN} R E M O V E - A - R E C O R D ${NC}"
+    echo ""
+    echo "${BOLD}Please select one of the following options:${NC}"
+    echo ""
+    echo "1) Remove records individually"
+    echo "2) Remove multiple records by County"
+    echo "3) Remove multiple records by instrumental speciality"
+    echo "4) Return to the previous menu"
+    read option
+    case $option in  
+        1)  loading
+            remove_single_record
+            ;;
+        2)  loading
+            remove_by_county
+            ;;
+        3)  loading
+            remove_by_speciality
+            ;;
+        4)  returning
+            break
+            ;;
+        *)  display_error
+            ;;
+    esac
 done
